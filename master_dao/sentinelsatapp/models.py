fcd5a76613ab662cca5af7040e092abaa2b21c92
daofullstack <daofullstack@gmail.com>
2021-11-27 09:32:48 +0000

    ajout de modele data

--- master_dao/sentinelsatapp/models.py
+++ master_dao/sentinelsatapp/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Properties(models.Model):
-	properties_id = models.AutoField(primary_key=True)
 	datatakesensingstart = models.CharField(max_length=100)
 	orbitnumber = models.IntegerField()
 	hv_order_tileid = models.CharField(max_length=100)
